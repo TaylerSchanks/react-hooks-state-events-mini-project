8b1a9bfd52882a77d1a137cce60986fb2533cff9
Tayler Schanks <taylerschanks2@gmail.com>
2023-08-15 13:17:59 -0500

    lab objective

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onSelectedCategory }) {
+
+    const showCategoryButtons = categories.map((category, index) => (
+         <button
+            key={index}
+            className={selectedCategory === category ? "selected" : ""}
+            onClick={() => onSelectedCategory(category)}>
+            {category}
+        </button>
+    ))
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {showCategoryButtons}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasks, setTaskList }) {
+
+    function handleDeleteTask(taskText) {
+        const updatedTaskList = tasks.filter((task) => task.text !== taskText)
+        setTaskList(updatedTaskList)
+    }
+
+    // const filteredTasks = selectedCategory === "All"
+    //     ? tasks
+    //     : tasks.filter((task) => task.category === selectedCategory)
+
+    const updatedTasks = tasks.map((task, index) => {
+        return <Task key={index} text={task.text} category={task.category} onDelete={handleDeleteTask} />
+    })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+        <ul>{updatedTasks}</ul>
     </div>
   );
 }
